1c371008be0b021248e2e529b6dda0cf2c782a45
Yaser Jarrar <yaser@grout.io>
2014-10-20 17:32:08 +0400

    Add version constraints for dependent packages

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.onUse(function (api) {
     api.versionsFrom('METEOR@0.9.1');
 
     api.use(['meteor-platform', 'http', 'accounts-base'], ['client', 'server']);
-    api.use(['mylar:basic-crypto', 'mylar:timing', 'mylar:async'], ['client', 'server']);
+    api.use(['mylar:basic-crypto@0.1.0', 'mylar:timing@0.1.0', 'mylar:async@0.1.0'], ['client', 'server']);
 
     api.addFiles(['graphmodel.js', 'certs.js', 'principal.js', 'collection_hook.js'], ['client', 'server']);
 
