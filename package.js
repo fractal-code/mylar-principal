bb3966d9bc118f48aa8a4313e41590989857bef9
Rami Jarrar <rami@grout.io>
2014-11-10 23:08:17 +0400

    Bump version to 0.2.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: "Principal graph",
     name: "mylar:principal",
-    version: "0.1.3",
+    version: "0.2.0",
     git: "https://github.com/gliesesoftware/mylar-principal.git"
 });
 
